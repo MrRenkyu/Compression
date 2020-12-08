0e8dd944b901fb348c2511d7d304ba15c09ea244
Thibault <thibaultderouin5@gmail.com>
2020-12-08 11:58:05 +0100

    RLE

--- Compression/src/Main/Main.java
+++ Compression/src/Main/Main.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.net.URI;
 import java.net.URL;
 
+import CompressClass.RLE;
 import FileManage.FileManager;
 
 public class Main {
@@ -13,7 +14,7 @@ public class Main {
 
 		FileManager fichier1 = new FileManager("text1.txt");
 		System.out.println(fichier1.getByteSize_InitialContentFile());
-		fichier1.setCompressContentFile("");
+		fichier1.setCompressContentFile(RLE.printRLE(fichier1.getInitialContentOfFile()));
 		System.out.println(fichier1.getByteSize_CompressFile());
 	}
 
